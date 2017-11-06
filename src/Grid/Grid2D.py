474fa08c8fb600f5990a7146b120fee17092f75f
g-benton <grbe0788@colorado.edu>
2017-11-05 17:22:54 -0700

    test

--- src/Grid/Grid2D.py
+++ src/Grid/Grid2D.py
@@ -74,7 +74,7 @@ class Grid2D:
                 if pred_in_new_posn:
                     # can't move into new space #
                     new_posn = curr_posn
-
+                    # dead line #
         return(new_posn)
 
 
