7993bc9f2e66ee3cfbb6f56a2274425d702e77ce
IanChar <ianchar4@gmail.com>
2017-11-07 20:27:37 -0700

    Added Grid Rules

--- src/Games/PredatorPrey/Predator.py
+++ src/Games/PredatorPrey/Predator.py
@@ -6,10 +6,6 @@ sys.path.append('../../Grid')
 from GridConstants import *
 from Actor import Actor
 
-## for Greg's Laptop ##
-import os
-os.chdir("/Users/greg/Dropbox/CSCI5622/CSCI5622/src/Games/PredatorPrey")
-
 
 class Predator(Actor):
     """Simple Predator class."""
--- src/Games/PredatorPrey/PredatorPreyRunner.py
+++ src/Games/PredatorPrey/PredatorPreyRunner.py
@@ -8,6 +8,7 @@ sys.path.append('../../Grid')
 from GridConstants import *
 from GridWorld import GridWorld
 from RunConditions import TimeLimitConditions
+from GridRules import SpawnNewActorRule
 
 def set_up():
     """Set up the world to run.
@@ -19,12 +20,18 @@ def set_up():
     wolf = Predator(2, (5, 5), dim = grid_dim)
     world.add_actor(sheep, (4, 5))
     world.add_actor(wolf, (5, 5))
+
+    def sheep_producer(actor_id, posn):
+        return Prey(actor_id, posn)
+
+    world.add_rule(SpawnNewActorRule(PREY, sheep_producer, time_interval=4))
+
     return world
 
 def run_sim():
     """Run the simulation."""
     world = set_up()
-    condition = TimeLimitConditions(10000)
+    condition = TimeLimitConditions(15)
     world.run_simulation(condition, True)
 
 if __name__ == '__main__':
